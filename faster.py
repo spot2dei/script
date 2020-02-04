225e07168373a5fa4ca875f8cf05ac1da138f09f
spot2dei <com12terate@gmail.com>
2020-02-04 20:13:48 +0900

    add faster.py

--- faster.py
+++ faster.py
@@ -3,6 +3,7 @@ import requests
 url = "http://suninatas.com/challenge/web07/web07.asp"
 url2 = "http://suninatas.com/challenge/web07/web07_1.asp"
 
+
 s = requests.Session()
 s.post(url)
 req = s.post(url2)
